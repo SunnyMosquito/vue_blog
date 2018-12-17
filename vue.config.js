650cf8f23277174df86e21defcbd3a156aeb5de5
SunnyMosquito <awen1018@hotmail.com>
2018-12-17 21:15:16 +0800

    nothing

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  baseUrl: '/',
+  baseUrl: "/",
   devServer: {
     port: 8080, // 端口号
     host: "0.0.0.0",
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000/api',
+      "/api": {
+        target: "http://127.0.0.1:8000/api",
         changeOrigin: true,
         pathRewrite: {
-          '^/api': ''
+          "^/api": ""
         }
       }
     }
   }
-}
+};
